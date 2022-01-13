e89c4d6e0cf83a937672b684c22fef3cd9e385c2
Elia.exe <eliasidori@outlook.it>
2022-01-13 14:40:57 +0100

    like button need to do modal form

--- photographer.js
+++ photographer.js
@@ -286,11 +286,47 @@ class Carousel{
         close.addEventListener("click", () =>{
             document.getElementById('works-lightbox').style.display = 'none';
         })
-
-
     }
 }
 
+class Like{
+    getNumbers(items){
+        let like = items.map(item => item.likes)
+        let price = items.map(item => item.price)
+        let box = document.getElementById("box")
+        let likes = document.createElement("span")
+        let prices = document.createElement("span")
+        likes.innerHTML = like.reduce((partial_sum, a) => partial_sum + a, 0);
+        prices.id = "prices"
+        likes.id = "likes"
+        box.insertAdjacentElement("afterbegin", prices)
+        box.insertAdjacentElement("afterbegin", likes)
+        box.innerHTML +='<i class="fas fa-heart" aria-label="likes"></i>'
+    }
+    ciao(data){
+        document.getElementById("prices").innerHTML
+        const id = window.location.search.split('id=')[1];
+        let photographer = data.photographers.filter(i => i.id == id)
+        document.getElementById("prices").innerHTML += (photographer[0].price) + "€/Jour"
+    }
+    click(){
+        let heart = document.querySelectorAll(".heart-btn")
+
+        heart.forEach(e => {
+            e.addEventListener("click", () => {
+                if(e.classList.contains("clicked")){
+                    e.classList.replace("fas", "far");
+                    e.classList.remove("clicked");
+                    ((e.previousElementSibling).children.item(0)).innerText = +(e.previousElementSibling).children.item(0).innerText -1
+                } else {
+                    e.classList.add("clicked");
+                    e.classList.replace("far", "fas");
+                    ((e.previousElementSibling).children.item(0)).innerText = +(e.previousElementSibling).children.item(0).innerText +1
+                }
+            })
+        });
+    }
+}
 function appDispatch() {
     new ApiFishEye().getDataFishEye().then((data) => {
         new DropDownMenu().sortValue();
@@ -304,9 +340,12 @@ function appDispatch() {
             new DropDownMenu().dropDown();
             new postFactory().bulidingPost(items);
             new Disposition();
-new PhotographerProfil().openModal();
-new PhotographerProfil().controllData();
-new Carousel().init(items);
+            new PhotographerProfil().openModal();
+            new PhotographerProfil().controllData();
+            new Carousel().init(items);
+            new Like().getNumbers(items);
+            new Like().ciao(data);
+            new Like().click()
 
             //PHOTOGRAPHER GALLERY & LIKES BOX
             //new MediaBuilder().photographersMedias(data);
